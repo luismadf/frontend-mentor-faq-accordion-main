d44c7f41a10fc7fd6ec07c2e09122a0d9a9f14d7
Luis De Freites <luisdefreites120@gmail.com>
2023-12-28 17:46:41 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img
+        src="/assets/background-pattern-desktop.svg"
+        className="logo"
+        alt="Vite logo"
+      />
+    </>
+  )
+}
+
+export default App
